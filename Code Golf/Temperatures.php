3faf8c29d348f86d99536d33400e7aa95efe09cb
AgentIvan <AgentIvan@gmail.com>
2017-04-26 03:06:03 +0300

    Update Temperatures.php

--- Code Golf/Temperatures.php
+++ Code Golf/Temperatures.php
@@ -1,1 +1,3 @@
 <?fgets(STDIN);$t=explode(' ',trim(fgets(STDIN)));sort($t);$m=999;foreach($t as $p)$m=abs($p)<=abs($m)?$p:$m;echo $m?$m:0;
+//you may use shorter of your code
+//fgets(STDIN);$t=fgetcsv(STDIN,0,' ');sort($t);$m=999;foreach($t as$p)abs($p)>abs($m)||$m=$p;echo$m?$m:0;
